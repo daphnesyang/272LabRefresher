962054184d5edfd4f337f121d7f288bd9ea200e6
Daphne Yang <daphney@andrew.cmu.edu>
2021-02-06 11:36:45 -0500

    Added name method, validators

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,5 +1,10 @@
 class Child < ApplicationRecord
     has_many :chores
     has_many :tasks, through: :chores
-    
+
+    validates_presence_of :first_name, :last_name
+
+    def name
+        first_name + " " + last_name
+    end
 end
