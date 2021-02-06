d95accc1b4686fbb8010b925ad7a9e997182ce35
Daphne Yang <daphney@andrew.cmu.edu>
2021-02-06 11:38:11 -0500

    Added scopes, all tests passing

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,6 +4,9 @@ class Child < ApplicationRecord
 
     validates_presence_of :first_name, :last_name
 
+    scope :alphabetical, -> { order('last_name', 'first_name') }
+    scope :active, -> { where(active: true) }
+
     def name
         first_name + " " + last_name
     end
